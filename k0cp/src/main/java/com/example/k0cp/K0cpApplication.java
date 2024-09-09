5e8a9b3910c5bfaae6dbe026d84da3172714e691
m0mf <157687643+m0mf@users.noreply.github.com>
2024-09-09 09:25:48 +0900

    Add files via upload

---
+++ k0cp/src/main/java/com/example/k0cp/K0cpApplication.java
@@ -0,0 +1,13 @@
+package com.example.k0cp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K0cpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K0cpApplication.class, args);
+	}
+
+}
---
+++ k0cp/src/test/java/com/example/k0cp/K0cpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.k0cp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K0cpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
